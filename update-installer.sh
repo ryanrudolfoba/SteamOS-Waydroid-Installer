db91a572c69d5154f1fb9c2998ef5947b4e7f276
ryanrudolfoba <98122529+ryanrudolfoba@users.noreply.github.com>
2024-10-28 11:51:58 -0400

    Delete update-installer.sh

--- update-installer.sh
+++
@@ -1,6 +0,0 @@
-cd ~/
-rm -rf ~/steamos-waydroid-installer
-git clone https://github.com/ryanrudolfoba/steamos-waydroid-installer
-cd ~/steamos-waydroid-installer
-chmod +x steamos-waydroid-installer.sh
-./steamos-waydroid-installer.sh
