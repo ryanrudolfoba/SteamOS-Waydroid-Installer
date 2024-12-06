8f89734bc601afeaf2c8be4754c16d6cbc593510
ryanrudolfoba <98122529+ryanrudolfoba@users.noreply.github.com>
2024-12-06 11:52:59 -0500

    automatic detection of screen resolution

--- steamos-waydroid-installer.sh
+++ steamos-waydroid-installer.sh
@@ -268,6 +268,8 @@ then
 	fi
 fi
 
+export RESOLUTION=\$(xdpyinfo | awk '/dimensions/{print \$2}')
+
 # stop and start the waydroid container
 sudo /usr/bin/waydroid-container-stop 
 sudo /usr/bin/waydroid-container-start
@@ -284,14 +286,14 @@ fi
 if [ -z "\$1" ]
 	then
 		# launch option not provided. launch Waydroid via cage and show the full ui right away
-		cage -- bash -c 'wlr-randr --output X11-1 --custom-mode 1280x800@60Hz ;	\\
+		cage -- bash -c 'wlr-randr --output X11-1 --custom-mode \$RESOLUTION@60Hz ; \\
 			/usr/bin/waydroid show-full-ui \$@ & \\
 
 			sleep 15 ; \\
 			sudo /usr/bin/waydroid-startup-scripts'
 	else
 		# launch option provided. launch Waydroid via cage but do not show full ui, launch the app from the arguments, then launch the full ui so it doesnt crash when exiting the app provided
-		cage -- env PACKAGE="\$1" bash -c 'wlr-randr --output X11-1 --custom-mode 1280x800@60Hz ; \\
+		cage -- env PACKAGE="\$1" bash -c 'wlr-randr --output X11-1 --custom-mode \$RESOLUTION@60Hz ; \\
 			/usr/bin/waydroid session start \$@ & \\
 
 			sleep 15 ; \\
