d6cf66c195e8b99d21e7715ba5f97018d45d3928
ryanrudolfoba <98122529+ryanrudolfoba@users.noreply.github.com>
2025-11-07 16:11:19 -0500

    we dont need this anymore

--- extras/spoofing.sh
+++
@@ -1,4 +0,0 @@
-#!/system/bin/sh
-
-# fix for CoD Mobile controller. This will simulate a gamepad Xbox Wireless Controller in addition to the Xbox 360 controller
-export CLASSPATH=/system/bin/classes.dex ; app_process / com.android.commands.hid.Hid
