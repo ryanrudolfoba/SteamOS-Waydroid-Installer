1622de488528a392b0c633abeeca71a1e2286cb0
ryanrudolfoba <98122529+ryanrudolfoba@users.noreply.github.com>
2026-06-25 08:54:41 -0400

    cleanup

--- extras/Waydroid-Updater.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo This will clone the latest version of the SteamOS Waydroid Installer script and perform an update.
-sleep 5
-cd ~/
-rm -rf ~/steamos-waydroid-installer
-git clone --depth=1 https://github.com/ryanrudolfoba/steamos-waydroid-installer
-cd ~/steamos-waydroid-installer
-chmod +x steamos-waydroid-installer.sh
-./steamos-waydroid-installer.sh
